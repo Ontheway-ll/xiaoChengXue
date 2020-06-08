fe9ce30dac1c0f46ba9ce8db84d69998023d41f2
liulei <1206821643@qq.com>
2020-06-08 09:57:40 +0800

    收货地址

--- src/pages/adress/index.vue
+++ src/pages/adress/index.vue
@@ -1,21 +1,21 @@
 <template>
   <view class="bigbox">
-    <view class="wrapper">
+    <view v-if="address" class="wrapper">
          <!-- 收货信息 -->
       <view class="shipment">
         <view class="pop">收货人：</view>
-         <text class="people">李德华</text> 
+         <text class="people">{{address.userName}}</text> 
       </view>
       <view class="shipment">
         <view class="tel">电话：</view>
-         <text class="telphone">13555555555</text> 
+         <text class="telphone">{{address.telNumber}}</text> 
       </view>
       <view class="shipment">
         <view class="address">收货地址：</view>
-         <text class="ads">天津西青区某某街道凯德大厦3楼666ffkkf</text> 
+         <text class="ads">{{detailAddress}}</text> 
       </view>
     </view>
-        <button @click="getadress"  type="primary">添加收货地址</button>
+        <button v-else  @click="getadress"  type="primary">添加收货地址</button>
   </view>
 </template>
 
@@ -35,6 +35,14 @@ export default {
           this.address=res  
         }
       })
+    }
+  },
+  computed:{
+    // 拼接详细地址
+    detailAddress(){
+      if (this.address) {
+      return this.address.provinceName+this.address.cityName+this.address.countyName+this.address.detailInfo
+      }
     }
   }
 }
