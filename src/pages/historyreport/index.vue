fdfcbc683861c2c8c165c4befbc17eb22ae5cb00
liulei <1206821643@qq.com>
2020-06-09 17:30:25 +0800

    登录弹窗

--- src/pages/historyreport/index.vue
+++ src/pages/historyreport/index.vue
@@ -1,6 +1,9 @@
 <template>
   <view>
-      历史上报
+      <view class="imgs">
+        <image src="../../static/imgs/task_icon_nodata.png"></image>
+        <view class="wu"><text class="bao">暂无上报</text></view>
+      </view>
   </view>
 </template>
 
@@ -10,6 +13,27 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .imgs{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    image{
+     height: 200rpx;
+     width: 200rpx;
+     margin-top: 400rpx;
+    }
+    .wu{
+      height: 100rpx;
+      width: 200rpx;
+      margin-top: 700rpx;
+      margin-left: -200rpx;
+      text-align: center;
+      .bao{
+        line-height: 100rpx;
+        font-size: 50rpx;
+        color: #757575;
+      }
+    }
+  }
 </style>
